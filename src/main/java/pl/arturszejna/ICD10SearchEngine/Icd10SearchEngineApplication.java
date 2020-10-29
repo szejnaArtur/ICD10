afc4ff0dfb07a3d8adc0eeef879e933dc1fed11c
Artur <szejnaartur@gmail.com>
2020-10-29 13:45:09 +0100

    create project

---
+++ src/main/java/pl/arturszejna/ICD10SearchEngine/Icd10SearchEngineApplication.java
@@ -0,0 +1,13 @@
+package pl.arturszejna.ICD10SearchEngine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Icd10SearchEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Icd10SearchEngineApplication.class, args);
+	}
+
+}
